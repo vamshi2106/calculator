a3cf9bc4a67e8aa5dadd59d269bdb2c9be62b4e2
vamshi2106 <aakulavamshikrishna93@gmail.com>
2018-05-20 19:49:07 -0400

    created

---
+++ calc.js
@@ -0,0 +1,28 @@
+function btnplus()
+{ 
+document.calculator.display.value +="+"; document.calculator.display.style.textAlign="right";
+}
+function btnsub()
+{ 
+document.calculator.display.value +="-"; document.calculator.display.style.textAlign="right";
+}
+function btnmult()
+{ 
+document.calculator.display.value +="*"; document.calculator.display.style.textAlign="right";
+}
+function btnmod()
+{ 
+document.calculator.display.value +="%"; document.calculator.display.style.textAlign="right";
+}
+function btndot()
+{ 
+document.calculator.display.value +="."; document.calculator.display.style.textAlign="right";
+}
+function btndiv()
+{ 
+document.calculator.display.value +="/"; document.calculator.display.style.textAlign="right";
+}
+function btnclear()
+{ 
+document.calculator.display.value +=""; document.calculator.display.style.textAlign="right";
+}
